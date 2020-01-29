539417e62bc8a2f654c7712f7613b7b1de6bc019
Nicolas Liapis <nicolas.liapis.dev@gmail.com>
2020-01-29 11:15:48 +0000

    v1.1.1 - removed extra actions

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,9 +1,5 @@
 export default function dataReducer(state = [], action) {
     switch(action.type) {
-        case 'UPDATE_DATA':
-            return [...state, {...action.data}];
-        case 'FILTER_DATA':
-            return [...state, {...action.newData}];
         case 'MATERIAL':
             return [...state, action.mat];
         default:
